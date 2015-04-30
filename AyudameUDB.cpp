3c7be9cbfec4e7ecdb3afeab0cc5dd6e6af0d712
keikkun4 <karla.hdz@alswblog.org>
2015-04-30 11:54:04 -0600

    Inicializacion de archivo principal Se creo el archivo con el que trabajara como archivo .cpp final y funcional

---
+++ AyudameUDB.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+using namespace std;
+
+/* AyudameUDB
+ 
+Este programa se trabajo en la Universidad Don Bosco, como proyecto de catedra por las estudiantes:
+1) Karla Lissette Hernández, HO122082, Ingeniería Biomédica.
+2) Karla Lissette Mayora Claros, MC140395, Ingeniería Biomédica.
+3) Allison Lisseth Vigil Argueta, VA130497, Ingeniería Ciencia de la Computación.
+4) Sara Abigail Fuentes Mejía, FM140843, Ingeniería Industrial.
+
+Descripcion del proyecto
+Crear un programa de apoyo y verificación para estudiantes de antiguo y nuevo ingreso que les facilite 
+el proceso de re-ingreso o ingreso y les guie paso a paso todo lo necesario para 
+ser estudiante de la Universidad Don Bosco de El Salvador. */
+
+char LoginUsuario (char);
+
+int main () {
+ LoginUsuario ()
+    
+}
+
+char LoginUsuario (char User){
+ //Aqui colocara su parte del trabajo Karla Hernandez 
+}
