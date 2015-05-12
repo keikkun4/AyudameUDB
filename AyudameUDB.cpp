4d5c3f567d840ff39f881e9ed24313ff03daaa06
chepecarlos <chepecarlos@alswblog.org>
2015-05-12 00:23:51 -0600

    agregando menu, y iniciando a crear ingreso de nuevo usuarios

--- AyudameUDB.cpp
+++ AyudameUDB.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <string.h>
-#include <windows.h>
+#include <stdlib.h>
+
 using namespace std;
 
 /* AyudameUDB
@@ -16,13 +18,67 @@ Crear un programa de apoyo y verificación para estudiantes de antiguo y nuevo i
 el proceso de re-ingreso o ingreso y les guie paso a paso todo lo necesario para 
 ser estudiante de la Universidad Don Bosco de El Salvador. */
 
-char LoginUsuario (char);
+#ifdef WIN32
+  #define LimpiarPantalla system( "cls" );
+#endif
+
+#ifdef __linux__
+  #define LimpiarPantalla system( "clear" );
+#endif
+
+char P;//Opcion del menu
+
+void CrearArchivo();//crea el archivo por si las dudas
+void LeerArchivo();
+
+void CrearUsuario();
 
 int main () {
- LoginUsuario ()
-    
+ CrearArchivo();
+ do{
+  cout<<"Menu"<<endl;
+  cout<<"1 - Ingresar"<<endl;
+  cout<<"2 - Crear nuevo usuario"<<endl;
+  cout<<"3 - Salir"<<endl;
+  cin>>P;
+  LimpiarPantalla;
+  switch ( P ){
+    case '1':
+    cout<<"Ingrese su usuario"<<endl;
+    break;
+    case '2':
+      CrearUsuario();
+    break;
+    case '3':
+     cout<<"Gracias por usar nuestro sistema"<<endl;
+    break;
+    default:
+     cout<<"No existe opcion, intente con otra"<<endl;
+    break;
+    }
+ }while(P != '3');
 }
 
-char LoginUsuario (char User){
- //Aqui colocara su parte del trabajo Karla Hernandez 
+void CrearArchivo(){
+  ofstream fs("Nombres.txt");
+  fs.close();
+}
+
+void CrearUsuario(){
+  string Nombre;
+  string Usuario;
+  string Contrasena;
+  char PP;
+  do{
+    cout<<"Nombre del estudiante"<<endl;  
+    cin>>Nombre;
+    cout<<"Nombre del estudiante"<<endl;
+    cin>>Usuario;
+    cout<<"Nombre del estudiante"<<endl; 
+    cin>>Contrasena;
+    cout<<"Nombre: "<<Nombre<<" Usuario: "<<Usuario<<" Contrasena: "<<Contrasena<<endl;
+    cout<<"Seguro Y/N"<<endl;
+    cin>>PP;
+  }while(PP != 'Y');
+  ifstream fe("nombre.txt");
 }
